2e54c01a138a4c799026834949259c07c9af0d5b
Diyou <diyou.dev@gmail.com>
2025-05-27 12:53:18 +0200

    Upped cmake version

--- Source/Android/app/build.gradle.kts
+++ Source/Android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.diyou.dotcmake"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -36,6 +36,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("../../../CMakeLists.txt")
+            version = "4.0.2"
         }
     }
     buildFeatures {
